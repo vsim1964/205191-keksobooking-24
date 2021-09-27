93d929e23e1ccc6615cce1c58acef6ab9e6c2e4e
Vladimir Siminenko <vsim1964@gmail.com>
2021-09-27 13:38:37 +0300

    Исправления по замечаниям наставника

--- js/main.js
+++ js/main.js
@@ -6,25 +6,25 @@ https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Ma
 https://learn.javascript.ru/number
 */
 
-function randomInteger(min, max) {
-  min = Math.ceil(min); // округление вверх
-  max = Math.floor(max); // округление вниз
-  if (max <= min) {
-    console.log('Значение числа max  должно быть больше значение числа min');
-  } else if (max < 0 || min < 0) {
-    console.log('Значения введенных чисел должны быть бльше или равны 0');
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
-console.log(randomInteger(50, 52));
-
-function randomFloat(min, max, n) {
+function getRandomInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
   if (max <= min) {
     console.log('Значение числа max  должно быть больше значение числа min');
   } else if (max < 0 || min < 0) {
     console.log('Значения введенных чисел должны быть больше или равны 0');
   }
-  let num = (Math.random() * (max - min)) + min;
-  return +num.toFixed(n);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(randomFloat(51.99998, 52, 5)); //Максимум и минимум включаются
+getRandomInteger(50, 52);
+
+function getRandomFloat(min, max, fraction) {
+  if (max <= min) {
+    console.log('Значение числа max  должно быть больше значение числа min');
+  } else if (max < 0 || min < 0) {
+    console.log('Значения введенных чисел должны быть больше или равны 0');
+  }
+  let randomNumber = (Math.random() * (max - min)) + min;
+  return +randomNumber.toFixed(fraction);
+}
+getRandomFloat(51.99998, 53, 5);
