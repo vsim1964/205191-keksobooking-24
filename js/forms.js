adfac7bfa506b4346071dc170b706bfe27d1dbbe
Siminenko <vsim1964@gmail.com>
2021-10-28 16:25:54 +0300

    КОНСТАНТЫ

--- js/forms.js
+++ js/forms.js
@@ -1,14 +1,21 @@
-// ! Валидация формы Заголовок == пункт 3.1 техзадания
 const MIN_NAME_LENGTH = 30;
 const MAX_NAME_LENGTH = 100;
+const ONE_ROOM_ERROR_MESSAGE = 'Количество гостей не может быть больше 1';
+const TWO_ROOM_ERROR_MESSAGE = 'Количество гостей не может быть больше 2';
+const THREE_ROOM_ERROR_MESSAGE = 'Количество гостей должно быть 1, 2 или 3';
+const HUNDRED_ROOM_ERROR_MESSAGE = '100 комнат - не длля гостей';
+const MAX_PRICE_ERROR_MESSAGE = 'Превышена максимальная цена - 1 000 000';
+const EMPTY_FIELD_ERROR_MESSAGE = 'Поле не должно быть пустым. Напишите цену, не меньше минимальной';
+
+// ! Валидация формы Заголовок == пункт 3.1 техзадания
 const titleInputElement = document.querySelector('#title');
 
 titleInputElement.addEventListener('input', () => {
   const valueLength = titleInputElement.value.length;
   if (valueLength < MIN_NAME_LENGTH) {
-    titleInputElement.setCustomValidity(`Добавьте ${  MIN_NAME_LENGTH - valueLength } симв.`);
+    titleInputElement.setCustomValidity(`Добавьте ${MIN_NAME_LENGTH - valueLength} симв.`);
   } else if (valueLength > MAX_NAME_LENGTH) {
-    titleInputElement.setCustomValidity(`Удалите лишние ${  valueLength - MAX_NAME_LENGTH } симв.`);
+    titleInputElement.setCustomValidity(`Удалите лишние ${valueLength - MAX_NAME_LENGTH} симв.`);
   } else {
     titleInputElement.setCustomValidity('');
   }
@@ -41,13 +48,12 @@ typeSelectElement.addEventListener('change', (evt) => {
 // ! Валидация формы Цена за ночь == пункт 3.2 техзадания
 priceInputElement.addEventListener('input', () => {
   const priceValue = priceInputElement.getAttribute('min');
-
   if (priceInputElement.validity.valueMissing) {
-    priceInputElement.setCustomValidity('Поле не должно быть пустым. Напишите цену, не меньше минимальной');
+    priceInputElement.setCustomValidity(EMPTY_FIELD_ERROR_MESSAGE);
   } else if (priceInputElement.validity.rangeUnderflow) {
     priceInputElement.setCustomValidity(`Порог минимальной цены - ${priceValue}`);
   } else if (priceInputElement.validity.rangeOverflow) {
-    priceInputElement.setCustomValidity('Превышена максимальная цена - 1 000 000');
+    priceInputElement.setCustomValidity(MAX_PRICE_ERROR_MESSAGE);
   } else {
     priceInputElement.setCustomValidity('');
   }
@@ -69,23 +75,21 @@ timeinSelectElement.addEventListener('change', (evt) => {
 });
 
 // ! Ограничение количества гостей  ==   пункт 3.6 техзадания
-const ONE_ROOM_ERROR_MNESSAGE = 'Количество гостей не может быть больше 1';
-const TWO_ROOM_ERROR_MNESSAGE = 'Количество гостей не может быть больше 2';
-const THREE_ROOM_ERROR_MNESSAGE = 'Количество гостей должно быть 1, 2 или 3';
-const HUNDRED_ROOM_ERROR_MNESSAGE = '100 комнат - не длля гостей';
+
 const roomNumberSelectElement = document.querySelector('#room_number');
 const capacitySelectElement = document.querySelector('#capacity');
 
 capacitySelectElement.addEventListener('change', (evt) => {
   if (roomNumberSelectElement.value === '1' && ['3', '2', '0'].includes(evt.target.value)) {
-    capacitySelectElement.setCustomValidity(ONE_ROOM_ERROR_MNESSAGE);
+    capacitySelectElement.setCustomValidity(ONE_ROOM_ERROR_MESSAGE);
   } else if (roomNumberSelectElement.value === '2' && ['3', '0'].includes(evt.target.value)) {
-    capacitySelectElement.setCustomValidity(TWO_ROOM_ERROR_MNESSAGE);
+    capacitySelectElement.setCustomValidity(TWO_ROOM_ERROR_MESSAGE);
   } else if (roomNumberSelectElement.value === '3' && ['0'].includes(evt.target.value)) {
-    capacitySelectElement.setCustomValidity(THREE_ROOM_ERROR_MNESSAGE);
+    capacitySelectElement.setCustomValidity(THREE_ROOM_ERROR_MESSAGE);
   } else if (roomNumberSelectElement.value === '100' && ['3', '2', '1'].includes(evt.target.value)) {
-    capacitySelectElement.setCustomValidity(HUNDRED_ROOM_ERROR_MNESSAGE);
+    capacitySelectElement.setCustomValidity(HUNDRED_ROOM_ERROR_MESSAGE);
   } else {
     capacitySelectElement.setCustomValidity('');
   }
+  capacitySelectElement.reportValidity();
 });
